796b4f23f12cee820b034c71256286df7c1b7a89
Vladimir Ivanenko <hi@vldmr.me>
2019-09-10 16:56:06 +0200

    nav + lang fixes

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,12 +1,17 @@
 import $ from 'jquery'
 
+const LANG_CONTROL_SEL = '#lang li'
+
 const switchToLang = (lang) => {
+  $(LANG_CONTROL_SEL).removeClass('active')
+  $(`${LANG_CONTROL_SEL}.${lang}`).addClass('active')
+
   $(`*[lang="${lang}"]`).show()
   $(`*[lang]:not([lang="${lang}"])`).hide()
 }
 
 switchToLang('de')
 
-$('#lang li').click((e) => {
+$(LANG_CONTROL_SEL).click((e) => {
   switchToLang($(e.target).text())
 })
