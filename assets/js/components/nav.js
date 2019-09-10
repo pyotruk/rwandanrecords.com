4b6be44b20dae8fa7c11906f79ac8d197b763336
Vladimir Ivanenko <hi@vldmr.me>
2019-09-10 17:02:06 +0200

    nav active fix

--- assets/js/components/nav.js
+++ assets/js/components/nav.js
@@ -3,7 +3,8 @@ import $ from 'jquery'
 $(document).ready(() => {
   $('nav #menu li').each((i, li) => {
     const href = $(li).find('a').attr('href').replace(/\.html/, '')
-    href === document.location.pathname
+    const path = document.location.pathname.replace(/\.html/, '')
+    href === path
       ? $(li).addClass('active')
       : $(li).removeClass('active')
   })
