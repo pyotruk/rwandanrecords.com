2c5690956b85aaa403f14c23cea72af6ef24674b
Vladimir Ivanenko <hi@vldmr.me>
2019-09-10 11:36:10 +0200

    modal

--- assets/js/components/record-modal.js
+++ assets/js/components/record-modal.js
@@ -1,13 +1,24 @@
 import $ from 'jquery'
 
-$(document).ready(() => {
-  $('#records ul li').first().click(() => {
-    $('#record-modal-overlay').show()
-    $('#record-modal').show()
-  })
+const OVERLAY_SEL = '#record-modal-overlay'
+const MODAL_SEL = '#record-modal'
 
-  $('#record-modal-overlay').click(() => {
-    $('#record-modal-overlay').hide()
-    $('#record-modal').hide()
+window.__rwandan_records__ = {
+  showModal: (videoUrl) => {
+    const iframe = $(MODAL_SEL).find('iframe')
+    iframe.attr('src', `${videoUrl}?autoplay=1`)
+    $(OVERLAY_SEL).show()
+
+    iframe.on('load', (e) => {
+      $(e.target).attr('src') && $(MODAL_SEL).show()
+    })
+  }
+}
+
+$(document).ready(() => {
+  $(OVERLAY_SEL).click(() => {
+    $(OVERLAY_SEL).hide()
+    $(MODAL_SEL).hide()
+    $(MODAL_SEL).find('iframe').attr('src', '')
   })
 })
