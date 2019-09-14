f47523a4bf3adab161b3f84a5329e18ce6897d36
Vladimir Ivanenko <hi@vldmr.me>
2019-09-14 15:16:11 +0200

    live fixes

--- assets/js/main-animation.js
+++ assets/js/main-animation.js
@@ -1,9 +1,14 @@
 import $ from 'jquery'
 
-$(window).bind('mousewheel', () => {
+const MAIN_TITLE_SEL = '#main-title'
+
+const animate = () => {
   if ($('body').attr('id') !== 'index') return
 
-  $('#main-title').removeClass('init')
+  $(MAIN_TITLE_SEL).removeClass('init')
   $('nav').removeClass('init')
   $('#records').removeClass('init')
-})
+}
+
+$(window).bind('mousewheel', () => animate())
+$(MAIN_TITLE_SEL).bind('click', () => animate())
