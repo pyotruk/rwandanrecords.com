cc5668f44a369d1e01b1cb63de18a54558e023ff
Vladimir Ivanenko <hi@vldmr.me>
2019-11-12 17:54:10 +0100

    animation works

--- assets/js/main-animation.js
+++ assets/js/main-animation.js
@@ -2,12 +2,29 @@ import $ from 'jquery'
 
 const MAIN_TITLE_SEL = '#main-title'
 
+const setScrollingEnabled = (isEnabled) => {
+  if (isEnabled) {
+    $('body').css('overflow', 'auto')
+  } else {
+    $('body').css('overflow', 'hidden')
+  }
+}
+
+const shouldAnimate = () => $(MAIN_TITLE_SEL).hasClass('init')
+
+if (shouldAnimate()) {
+  setScrollingEnabled(false)
+}
+
 const animate = () => {
   if ($('body').attr('id') !== 'index') return
 
   $(MAIN_TITLE_SEL).removeClass('init')
   $('nav').removeClass('init')
   $('#records-desktop').removeClass('init')
+  $('#records-mobile').removeClass('init')
+
+  setTimeout(() => setScrollingEnabled(true), 1500)
 }
 
 $(window).bind('mousewheel', () => animate())
